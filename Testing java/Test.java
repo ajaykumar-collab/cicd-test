6a1355e209baf88609503d69eefca84b7d148211
ajaykumar-collab <ajaykumarfor007@gmail.com>
2025-11-20 15:47:42 +0530

    Update Test.java

--- Testing java/Test.java
+++ Testing java/Test.java
@@ -2,6 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Checking pull request");
-        System.out.println("Merge conflict message 2");
+        System.out.println("Merge conf 33");
     }
 }
